ee3d354f5993290ff2777bc580c0a6c966f285ff
dreamdev2427 <protodev531@gmail.com>
2023-08-08 12:04:16 +0300

    fff

--- src/components/authentication/SignIn.tsx
+++ src/components/authentication/SignIn.tsx
@@ -51,7 +51,7 @@ const SignIn = ({ switchToSignUp, closeModal }): JSX.Element => {
   return (
     <>
       <div
-        className="fixed top-0 left-0 h-screen w-screen bg-opacity-75 bg-gray-800 z-40"
+        className="fixed top-0 left-0 h-screen w-screen bg-opacity-75 bg-gray-800 z-1000"
         onClick={closeModal}
         aria-hidden={true}
       />
